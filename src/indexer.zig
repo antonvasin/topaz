6f000b34af3e209ab2ea687a153a4a14ae93ec78
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2026-06-05 22:08:05 +0100

    Wrap DB with Indexer

---
+++ src/indexer.zig
@@ -0,0 +1,24 @@
+const DB = @import("./db.zig").DB;
+const std = @import("std");
+const log = std.log.scoped(.indexer);
+
+pub const Indexer = struct {
+    db: DB,
+
+    pub fn init(db_name: [:0]const u8) !Indexer {
+        log.debug("Using DB file \"{s}\"\n", .{db_name});
+        var db = try DB.open(db_name);
+
+        // path             TEXT canonical document name
+        // content          TEXT text content
+        // created_at       TEXT ISO-8601
+        // updated_at       TEXT ISO-8601
+        try db.exec("CREATE TABLE IF NOT EXISTS documents(path TEXT UNIQUE NOT NULL, content TEXT, created_at TEXT NOT NULL, updated_at TEXT NOT NULL)", null, null);
+
+        return .{ .db = db };
+    }
+
+    pub fn deinit(self: *Indexer) void {
+        self.db.close();
+    }
+};
